992d877cf27af9418c8874cf9739e9cae812ecbb
Bartek Bajda <bart.bajda@thewidlarzgroup.com>
2020-03-04 20:32:21 +0100

    prepare styles and static example

--- src/part4/components/SearchBar.tsx
+++ src/part4/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   inputValue: string;
@@ -8,27 +8,53 @@ interface Props {
 const SearchBar = ({ inputValue, setInputValue }: Props) => {
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget;
-
     setInputValue(value);
   };
 
   return (
-    <input
-      placeholder='Type in the full name'
-      style={styles.input}
-      value={inputValue}
-      onChange={handleChange}
-    />
+    <div style={styles.container}>
+      <input
+        placeholder='Type in the full name'
+        style={styles.input}
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <div>
+        <div style={styles.optionItem}>Contact 1</div>
+        <div style={styles.optionItem}>Contact 2</div>
+        <div style={styles.optionItem}>Contact 3</div>
+        <div style={styles.optionItem}>Contact 4</div>
+        <div style={styles.optionItem}>Contact 5</div>
+      </div>
+    </div>
   );
 };
 
 const styles = {
-  input: {
-    display: 'block',
+  container: {
     margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column' as any,
+    height: 'auto',
     width: 400,
+    backgroundColor: '#90EE90'
+  },
+  input: {
+    display: 'inline-block',
     padding: 15,
-    fontSize: 20
+    fontSize: 20,
+    border: '1px solid #000'
+  },
+  optionsDiv: {
+    width: '100%',
+    border: '1px solid #000'
+  },
+  optionItem: {
+    width: '100%',
+    height: 60,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 };
 
